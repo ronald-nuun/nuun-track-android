299ceb52c4ff751a9be1d5e5a6ba37a0977db71c
Faradyna Rahma <fara@nuun.tech>
2025-09-04 14:29:31 +0700

    [FEAT] Added navigation logger

--- app/src/main/java/com/nuun/track/utility/consts/NuunTag.kt
+++ app/src/main/java/com/nuun/track/utility/consts/NuunTag.kt
@@ -3,4 +3,6 @@ package com.nuun.track.utility.consts
 object NuunTag {
     const val DEFAULT = "Nuun-Track"
     const val HTTP = "Nuun-HTTP"
+    const val NAVIGATION = "Nuun-Navigation"
+    const val NAVIGATION_DESTINATION = "Nuun-NavDestination"
 }
---
+++ app/src/main/java/com/nuun/track/utility/logger/NavigationLogger.kt
@@ -0,0 +1,5 @@
+package com.nuun.track.utility.logger
+
+import com.nuun.track.utility.consts.NuunTag
+
+class NavigationLogger(defaultTag: String = NuunTag.NAVIGATION) : BaseLogger(defaultTag)
